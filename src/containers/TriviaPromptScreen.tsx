7612ba23482f9b8cdc615c3780407f5bff509a1a
Adolph C <adolph.celestin@gmail.com>
2018-03-02 09:57:58 -0500

    Adds store references to prompt and results screen.

--- src/containers/TriviaPromptScreen.tsx
+++ src/containers/TriviaPromptScreen.tsx
@@ -1,9 +1,17 @@
 import React = require("react");
 import ReactDOM = require("react-dom");
+import { observer } from "mobx-react";
+import { getRootStore } from "../store";
 
+@observer
 class TriviaPromptScreen extends React.Component<{}, {}> {
+    private userStore = getRootStore().userStore;
+    private triviaStore = getRootStore().triviaStore;
+
     public render() {
-        return <div>TriviaPromptScreen</div>;
+        return <div>
+            TriviaPromptScreen
+        </div>;
     }
 }
 
--- src/containers/TriviaResultsScreen.tsx
+++ src/containers/TriviaResultsScreen.tsx
@@ -1,7 +1,13 @@
 import React = require("react");
 import ReactDOM = require("react-dom");
+import { observer } from "mobx-react";
+import { getRootStore } from "../store";
 
+@observer
 class TriviaResultsScreen extends React.Component<{}, {}> {
+    private userStore = getRootStore().userStore;
+    private triviaStore = getRootStore().triviaStore;
+
     public render() {
         return <div>TriviaResultsScreen</div>;
     }
