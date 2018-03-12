334e42a05db8fab769b8848c3fa5fc646544a5b8
Adolph C <adolph.celestin@gmail.com>
2018-03-12 03:28:42 -0400

    Makes some changes to styling throughout the app

--- src/index.tsx
+++ src/index.tsx
@@ -16,23 +16,20 @@ const store = new RootStore(API);
 store.userStore.loadAuthInfo(); // log us in 'n stuff
 setRootStore(store);
 
-// this is just so I can keep track of them
-const openColor = {
-    orange4: "#ffa94d",
-    orange5: "#ff922b",
-    orange6: "#fd7e14",
-    orange7: "#f76707",
-
-    yellow5: "#fcc419",
-};
-
 const theme = createMuiTheme({
     palette: {
-        type: "dark",
+        type: "light",
         primary: {
-            light: openColor.orange4,
-            main: openColor.orange5,
-            dark: openColor.orange6,
+            light: "#757de8",
+            main: "#3f51b5",
+            dark: "#002984",
+            contrastText: "#ffffff",
+        },
+        secondary: {
+            light: "#ffa270",
+            main: "#ff7043",
+            dark: "#c63f17",
+            contrastText: "#000000",
         },
     },
 });
