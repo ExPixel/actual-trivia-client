3df117e635d001b0a9824fbe0b719cdd99baf810
Adolph C <adolph.celestin@gmail.com>
2018-03-12 22:53:35 -0400

    Adds black version of the banner.

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,13 +1,24 @@
 import React = require("react");
 import ReactDOM = require("react-dom");
-import bannerImage =  require("../static/img/actual-trivia-banner.svg");
+import bannerImageWhite = require("../static/img/actual-trivia-banner-white.svg");
+import bannerImageBlack = require("../static/img/actual-trivia-banner-black.svg");
 
 const DEFAULT_BRANDING_WIDTH: number = 512;
 
 export interface IProps {
-    width?: number;
+    width?: number | string;
     style?: React.CSSProperties;
+    black?: boolean;
 }
 
-const Branding = ({width, style}: IProps) => (<img src={bannerImage} style={style} width={width || DEFAULT_BRANDING_WIDTH}/>);
+const widthpx = (w: number | string) => typeof w === "string" ? w : w + "px";
+
+const Branding = ({width, style, black}: IProps) => (
+    <img
+        src= {black ? bannerImageBlack : bannerImageWhite}
+        style= {
+            Object.assign({ width: widthpx(width || DEFAULT_BRANDING_WIDTH) }, style)
+        } />
+);
+
 export default Branding;
